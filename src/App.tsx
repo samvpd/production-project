c356aa7eafb97d97b31661938c8083b441d52272
Sam.vpd <sam.vpd@gmail.com>
2023-11-19 23:18:49 +0300

    styles and themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { Suspense } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import './index.scss';
+import { useTheme } from './theme/useTheme';
+import './styles/index.scss';
 
 export const App = () => {
+	const { theme, toggleTheme } = useTheme();
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
+			<button type='button' onClick={toggleTheme}>
+				TOGGLE
+			</button>
 			<Link to='/'>Главная</Link>
 			<Link to='/about'>О сайте</Link>
 			<Suspense fallback={<div>Loading...</div>}>
