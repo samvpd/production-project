ff62706c646c2da80d46509b92b09f10102955d7
Sam.vpd <sam.vpd@gmail.com>
2023-11-25 00:27:34 +0300

    Router provider and config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import { Suspense } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { MainPage } from 'pages/MainPage';
-import { AboutPage } from 'pages/AboutPage';
+
 import './styles/index.scss';
 
 export const App = () => {
@@ -16,13 +15,7 @@ export const App = () => {
 			</button>
 			<Link to='/'>Главная</Link>
 			<Link to='/about'>О сайте</Link>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route path='/' element={<MainPage />} />
-					<Route path='/about' element={<AboutPage />} />
-					<Route />
-				</Routes>
-			</Suspense>
+			<AppRouter />
 		</div>
 	);
 };
