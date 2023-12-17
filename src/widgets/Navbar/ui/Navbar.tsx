24e9013cfcc76fceac54ecb303359fa1cf45537f
Sam.vpd <sam.vpd@gmail.com>
2023-12-17 21:05:55 +0300

    remove translate from navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 
@@ -8,18 +7,15 @@ interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-    const { t } = useTranslation();
-    return (
-        <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
-                    {t('Главная страница')}
-                </AppLink>
-                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-                    {t('О Сайте')}
-                </AppLink>
-            </div>
+export const Navbar = ({ className }: NavbarProps) => (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+        <div className={cls.links}>
+            <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
+                Главная
+            </AppLink>
+            <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+                О сайте
+            </AppLink>
         </div>
-    );
-};
+    </div>
+);
